4671f8aed7c30fe456b772805fa9d031f11edd2f
Aaranya Maskey <np03cs4a220415@heraldcollege.edu.np>
2023-05-04 19:36:51 +0545

    weather history part added

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-    include 'connection.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +7,7 @@
     <title>Weather Boy</title>
     <script src="https://kit.fontawesome.com/1ae1db6a1d.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css">
 </head>
 <body>
@@ -25,20 +23,132 @@
                 
             </div>
         </div>   
-    
-        <div class="title" id="title">
-         
-        </div>
-         
-        <div class="content" id="weather">
 
-           
+        <div class="title" id="title">
+            <h1>KTM</h1>
+            <h2>2nd ap</h2>
         </div>
+         <div class="weather-container">
+            <div class="content" id="weather">
+                <div class="row-1">
+                    <img src="">
+                    <div class="item-title">Rain</div>
+                </div>
+            
+                <div class="row-2">
+            
+                    <div class="item">
+                        <i class="fa-solid fa-temperature-three-quarters"></i>
+                        <div class="item-title">
+                            Temperature
+                        </div>
+                        <div class="item-content">
+                            2°C
+                        </div>
+                    </div>
+            
+                    <div class="item">
+                        <i class="fa-solid fa-gauge"></i>
+                        <div class="item-title">
+                            Pressure
+                        </div>
+                        <div class="item-content">
+                          2 mb
+                        </div>
+                    </div>
+            
+                    <div class="item">
+                        <i class="fa-solid fa-wind"></i>
+                        <div class="item-title">
+                            Windspeed
+                        </div>
+                        <div class="item-content">
+                            2km/h
+                        </div>
+                    </div>
+            
+                    <div class="item">
+                        <i class="fa-solid fa-droplet"></i>
+                        <div class="item-title">
+                            Humidity
+                        </div>
+                        <div class="item-content">
+                            2%
+                        </div>
+                    </div>
+            
+                    </div>
+            </div>
+               
+            <div class="weather-history">
+                <div class="day">
+                    <div class="item-title">
+                        Sunday
+                    </div>
+                    <div class="item-content">
+                        2°C
+                    </div>
+                </div>
+
+                <div class="day">
+                    <div class="item-title">
+                        Sunday
+                    </div>
+                    <div class="item-content">
+                        Weather | 2°C
+                    </div>
+                </div>
+
+                <div class="day">
+                    <div class="item-title">
+                        Sunday
+                    </div>
+                    <div class="item-content">
+                        Weather | 2°C
+                    </div>
+                </div>
+
+                <div class="day">
+                    <div class="item-title">
+                        Sunday
+                    </div>
+                    <div class="item-content">
+                        Weather | 2°C
+                    </div>
+                </div>
+
+                <div class="day">
+                    <div class="item-title">
+                        Sunday
+                    </div>
+                    <div class="item-content">
+                        Weather | 2°C
+                    </div>
+                </div>
+
+                <div class="day">
+                    <div class="item-title">
+                        Sunday
+                    </div>
+                    <div class="item-content">
+                        Weather | 2°C
+                    </div>
+                </div>
+
+                <div class="day">
+                    <div class="item-title">
+                        Sunday
+                    </div>
+                    <div class="item-content">
+                       Weather | 2°C
+                    </div>
+                </div>
+            </div>
+         </div>
+        
         <div class="footer">
             &#169; Made by Aaranya Maskey
         </div>
     </div>
-   
-    <script src="weather.js"></script>
 </body>
 </html>
